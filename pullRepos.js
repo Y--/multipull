0ef84b21dcf4af3a39331a1a331a59105260d1d8
Yves Le Maout <yves@sutoiku.com>
2016-07-29 15:17:51 -0400

    Put all functions at the end

--- pullRepos.js
+++ pullRepos.js
@@ -41,6 +41,77 @@ CliTable.prototype.removeEmptyColumns = function() {
   this.options.head.unshift(shifted);
 };
 
+async.parallel(repos.map(r => done => processRepo(r, done)), (err, res) => {
+  if (err) { return handleErr(err); }
+
+  const table = new CliTable({
+      head : ['',
+        'Current', 'Tracking', 'S', '??', 'M', 'D', 'A', 'C',
+        'Files', 'Changes', 'Insertions', 'Deletions']
+  });
+
+  const errors = [];
+  for (const [i, repo] of repos.entries()) {
+    if (repo !== res[i].repo) { return handleErr(new Error('Unordered results')); }
+
+    const { pull, status, stash } = res[i];
+    const pos = [];
+    if (status.ahead)  { pos.push('+' + status.ahead); }
+    if (status.behind) { pos.push('-' + status.behind); }
+
+    const statuses = ['not_added', 'modified', 'deleted', 'created', 'conflicted'].map(oneOrCountFactory(status));
+    let pulls;
+    if (!pull.error) {
+      const suffix = pull.files.find(isNative) ? ' (n)' : '';
+      pulls = [oneOrCountFactory(pull)('files') + suffix, pull.summary.changes || '', pull.summary.insertions || '', pull.summary.deletions || ''];
+    } else {
+      pulls = [ 'error', '-', '-', '-' ];
+      errors.push(pull.error);
+    }
+
+    const elt = {};
+    const current  = status.current  === 'master'        ? '' : (status.current  || '*** none ***');
+    const tracking = status.tracking === 'origin/master' ? '' : (status.tracking || '*** none ***');
+    elt[repo] = [ current + pos.join('/'), tracking, stash.total || '', ...statuses, ...pulls];
+    if (elt[repo].find(e => e !== '')) {
+      table.push(elt);
+    }
+  }
+
+  table.removeEmptyColumns();
+  console.log(table.toString());
+
+  if (errors.length) {
+    console.error("%d error(s) occured while pulling repos :", errors.length);
+  }
+  for (const err of errors) {
+    console.error(err);
+  }
+});
+
+function isNative(f) {
+  return f.endsWith('cc') || f.endsWith('hh');
+}
+
+function oneOrCountFactory(obj) {
+  return (s) => {
+    const arr = obj[s];
+    if (!arr.length) { return ''; }
+    if (arr.length === 1) { return arr[0]; }
+    return arr.length;
+  };
+}
+
+function handleErr(err) {
+  if (!err.message || !err.stack) {
+    console.error(err);
+  } else {
+    console.error(err.message);
+    console.error(err.stack);
+  }
+  process.exit(-1);
+}
+
 function processRepo(repo, done) {
   const sg = simpleGit(path.join(rootDir, repo)).silent(true);
   return sg.fetch(err => {
@@ -143,73 +214,3 @@ function isLocalClean(status) {
   return !['modified', 'deleted', 'created', 'conflicted'].map(k => status[k]).filter(v => !!v.length).length;
 }
 
-async.parallel(repos.map(r => done => processRepo(r, done)), (err, res) => {
-  if (err) { return handleErr(err); }
-
-  const table = new CliTable({
-      head : ['',
-        'Current', 'Tracking', 'S', '??', 'M', 'D', 'A', 'C',
-        'Files', 'Changes', 'Insertions', 'Deletions']
-  });
-
-  const errors = [];
-  for (const [i, repo] of repos.entries()) {
-    if (repo !== res[i].repo) { return handleErr(new Error('Unordered results')); }
-
-    const { pull, status, stash } = res[i];
-    const pos = [];
-    if (status.ahead)  { pos.push('+' + status.ahead); }
-    if (status.behind) { pos.push('-' + status.behind); }
-
-    const statuses = ['not_added', 'modified', 'deleted', 'created', 'conflicted'].map(oneOrCountFactory(status));
-    let pulls;
-    if (!pull.error) {
-      const suffix = pull.files.find(isNative) ? ' (n)' : '';
-      pulls = [oneOrCountFactory(pull)('files') + suffix, pull.summary.changes || '', pull.summary.insertions || '', pull.summary.deletions || ''];
-    } else {
-      pulls = [ 'error', '-', '-', '-' ];
-      errors.push(pull.error);
-    }
-
-    const elt = {};
-    const current  = status.current  === 'master'        ? '' : (status.current  || '*** none ***');
-    const tracking = status.tracking === 'origin/master' ? '' : (status.tracking || '*** none ***');
-    elt[repo] = [ current + pos.join('/'), tracking, stash.total || '', ...statuses, ...pulls];
-    if (elt[repo].find(e => e !== '')) {
-      table.push(elt);
-    }
-  }
-
-  table.removeEmptyColumns();
-  console.log(table.toString());
-
-  if (errors.length) {
-    console.error("%d error(s) occured while pulling repos :", errors.length);
-  }
-  for (const err of errors) {
-    console.error(err);
-  }
-});
-
-function isNative(f) {
-  return f.endsWith('cc') || f.endsWith('hh');
-}
-
-function oneOrCountFactory(obj) {
-  return (s) => {
-    const arr = obj[s];
-    if (!arr.length) { return ''; }
-    if (arr.length === 1) { return arr[0]; }
-    return arr.length;
-  };
-}
-
-function handleErr(err) {
-  if (!err.message || !err.stack) {
-    console.error(err);
-  } else {
-    console.error(err.message);
-    console.error(err.stack);
-  }
-  process.exit(-1);
-}
