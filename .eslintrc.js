530903ce2e2748c044eec05f85416e4677b50e35
Yves Le Maout <yves@sutoiku.com>
2018-02-18 22:55:19 -0500

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
-'use strict';
-
-const fs       = require('fs');
-const jshintrc = JSON.parse(fs.readFileSync(`${__dirname}/.jshintrc`, 'utf8'));
-const globals  = jshintrc.predef.reduce((acc, v) => { acc[v] = true; return acc; }, {});
-
 module.exports = {
-  extends : 'eslint:recommended',
-  env     : { es6: true, node: true, browser: true },
-  globals : globals,
-  rules   : {
-    'linebreak-style' : [ 2, 'unix' ],
-    'semi'            : [ 2, 'always' ],
-    'prefer-const'    : [ 'error', { destructuring: 'any' } ],
-    'prefer-spread'   : [ 'error'],
-    'no-unmodified-loop-condition' : [ 'error' ],
-    'prefer-rest-params' : [ 'error' ]
-  }
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {"ecmaVersion": 2017},
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
