4b157565448f74129f9adb835a5a9c09cdf83f12
Yves <yves@sutoiku.com>
2019-08-21 12:18:02 -0400

    Ignore `require-atomic-updates`

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
     "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error"
+    "jest/valid-expect": "error",
+    "require-atomic-updates": 0
   }
 };
